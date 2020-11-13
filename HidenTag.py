6d26c119879b2abe1feef572a375c4c0f19572f7
VolfBeats <73528902+VolfBeats@users.noreply.github.com>
2020-11-13 09:23:23 +0200

    Update HidenTag.py

--- HidenTag.py
+++ HidenTag.py
@@ -11,7 +11,11 @@ class HidenTagMod(loader.Module):
     async def tagcmd(self, message):
         """Использование: .tag <@> <текст (по желанию)>."""
         args = utils.get_args_raw(message).split(' ')
-        tag = '┈┏┓┏┳━┳┓┏┓┏━━┓┈\n ┈┃┃┃┃┏┛┃┃┃┃┏┓┃┈\n ┈┃┗┛┃┗┓┃┃┃┃┃┃┃┈\n ┈┃┏┓┃┏┛┃┃┃┃┃┃┃┈\n ┈┃┃┃┃┗┓┗┫┗┫╰╯┃┈\n ┈┗┛┗┻━┻━┻━┻━━┛┈\n'
+        tag = ("┈┏┓┏┳━┳┓┏┓┏━━┓┈\n "┈┃┃┃┃┏┛┃┃┃┃┏┓┃┈\n"
+               "┈┃┗┛┃┗┓┃┃┃┃┃┃┃┈\n"
+               "┈┃┏┓┃┏┛┃┃┃┃┃┃┃┈\n"
+               "┈┃┃┃┃┗┓┗┫┗┫╰╯┃┈\n"
+               "┈┗┛┗┻━┻━┻━┻━━┛┈\n")
         try:
             user = await message.client.get_entity(args[0])
         except:
